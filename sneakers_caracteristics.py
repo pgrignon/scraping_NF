099b02fee16b240ea4a9d2624e275233f8a27ed9
Paul <p.grignon33@gmail.com>
2021-08-08 19:08:34 +0200

    [ENH] Sneaker caracteristics

---
+++ sneakers_caracteristics.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup  # type: ignore
+
+
+def get_sneaker_price(sneaker: str) -> float:
+    """
+    Get the price of a sneaker
+    ---------------
+    Parameters:
+        sneaker: extract from source code zoomed on a sneaker
+    Returns:
+        The price of a sneaker
+    """
+    price: str = (
+        sneaker.find(class_="grid-product__price") # type: ignore
+        .get_text()
+        .replace("\n", "")
+        .replace("€", "")
+    )
+    if len(price.split("Sale price")) > 1:
+        return float(price.split("Sale price")[1])
+    else:
+        return float(price)
+
+
+def get_sneaker_name(sneaker: str) -> str:
+    """
+    Get the name of a sneaker
+    --------------
+    Parameters:
+        sneaker: extract from source code zoomed on a sneaker
+    Returns:
+        The name of a sneaker
+    """
+    return sneaker.find(
+        class_="grid-product__title grid-product__title--body"
+    ).get_text() # type: ignore
+
+
+def get_sneaker_href(sneaker: str) -> str:
+    """
+    Get the href link to the sneaker product
+    --------------
+    Parameters:
+        sneaker: extract from source code zoomed on a sneaker
+    Returns:
+        The href link to the sneaker product
+    """
+    return sneaker.find("a").get("href") # type: ignore
