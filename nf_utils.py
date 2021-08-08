2fb3a23e3f203450e455318494189c70f7c7f069
Paul <p.grignon33@gmail.com>
2021-08-08 18:36:47 +0200

    [ENH] NF utils

---
+++ nf_utils.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup  # type: ignore
+from global_variables import URL
+import requests
+
+
+def get_number_of_pages_sneakers(url: str = URL) -> int:
+    """
+    Get the number of pages for the sneaker category
+    --------------
+    Parameters:
+        url: the url of the sneaker category
+    Returns:
+        The number of pages for the sneaker category
+    """
+    req = requests.get(URL)
+    html = req.text
+    soup = BeautifulSoup(html, "html.parser")
+    pages = soup.find(class_="pagination").get_text()
+    return pages.replace("\n", "").split(" ")[-2]
